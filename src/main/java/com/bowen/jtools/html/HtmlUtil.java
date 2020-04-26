5a204cc0b4ae48066194cfb6d0821c115630e2eb
江博文 <jiangbowen@xiaomi.com>
2020-04-26 17:09:43 +0800

    more function

--- src/main/java/com/bowen/jtools/html/HtmlUtil.java
+++ src/main/java/com/bowen/jtools/html/HtmlUtil.java
@@ -1,5 +1,7 @@
 package com.bowen.jtools.html;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +11,26 @@ import java.util.regex.Pattern;
  * On 19-5-23
  */
 public class HtmlUtil {
+
+    /**
+     * 根据正则 reg 匹配所有分组
+     *
+     * @param str
+     * @param regEx
+     * @return
+     */
+    public static List<String> matchAllGroupByReg(String str, String regEx){
+
+        Pattern p = Pattern.compile(regEx, Pattern.DOTALL);
+        Matcher matcher = p.matcher(str);
+        List<String> groupList = new ArrayList<>();
+
+        while (matcher.find()) {
+            groupList.add(matcher.group());
+        }
+
+        return groupList;
+    }
 
     /**
      * 根据正则 reg 匹配文本
@@ -21,7 +43,8 @@ public class HtmlUtil {
         Pattern p = Pattern.compile(regEx);
         Matcher matcher = p.matcher(str);
         if (matcher.find()){
-            return HtmlUtil.filterHTMLCharacter(matcher.group());
+            // return HtmlUtil.filterHTMLCharacter(matcher.group());
+            return matcher.group();
         }
 
         return "";
@@ -36,21 +59,26 @@ public class HtmlUtil {
     public static String filterHTMLCharacter(String htmlStr){
 
         // 去除 script、style、以及所有html标签
-        String regEx_script="<script[^>]*?>[\\s\\S]*?<\\/script>";
-        String regEx_style="<style[^>]*?>[\\s\\S]*?<\\/style>";
-        String regEx_html="<[^>]+>"; //定义HTML标签的正则表达式
+        String regEx_script = "<script[^>]*?>[\\s\\S]*?<\\/script>";
+        String regEx_style = "<style[^>]*?>[\\s\\S]*?<\\/style>";
+        String regEx_html = "<[^>]+>"; //定义HTML标签的正则表达式
+        String regEx_blank = "\n*  *   *    *";
 
-        Pattern p_script=Pattern.compile(regEx_script,Pattern.CASE_INSENSITIVE);
-        Matcher m_script=p_script.matcher(htmlStr);
-        htmlStr=m_script.replaceAll("");
+        Pattern p_script = Pattern.compile(regEx_script, Pattern.CASE_INSENSITIVE);
+        Matcher m_script = p_script.matcher(htmlStr);
+        htmlStr = m_script.replaceAll("");
 
-        Pattern p_style=Pattern.compile(regEx_style,Pattern.CASE_INSENSITIVE);
-        Matcher m_style=p_style.matcher(htmlStr);
-        htmlStr=m_style.replaceAll("");
+        Pattern p_style = Pattern.compile(regEx_style, Pattern.CASE_INSENSITIVE);
+        Matcher m_style = p_style.matcher(htmlStr);
+        htmlStr = m_style.replaceAll("");
 
-        Pattern p_html=Pattern.compile(regEx_html,Pattern.CASE_INSENSITIVE);
-        Matcher m_html=p_html.matcher(htmlStr);
-        htmlStr=m_html.replaceAll("");
+        Pattern p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE);
+        Matcher m_html = p_html.matcher(htmlStr);
+        htmlStr = m_html.replaceAll("");
+
+        Pattern p_lank = Pattern.compile(regEx_blank, Pattern.CASE_INSENSITIVE);
+        Matcher m_blank = p_lank.matcher(htmlStr);
+        htmlStr = m_blank.replaceAll("");
 
         return htmlStr.trim();
     }
